2eeb6ce2644dd4dfe676084be0cb5cb14bf04d49
unknown <db@teamtroxler.com>
2020-08-26 13:37:24 -0400

    Revise file name

--- backup.py
+++ backup.py
@@ -2,14 +2,14 @@ import json
 from pyntc import ntc_device as NTC
 import getpass
 
-f = open('IPlist.txt', 'r')
+f = open('iplist.txt', 'r')
 
 hosts = f.read()
 hosts = hosts.split()
 
 pw = getpass.getpass()
 for each in hosts:
-	try:	
+	try:
 		file = open(each+".txt", "w+")
 		print("\n", each, "-- writing backup locally")
 		x = NTC(host=each, username='dtroxler', password=pw, device_type='cisco_ios_ssh')
@@ -17,4 +17,3 @@ for each in hosts:
 		file.write(backup)
 	except:
 		print(each, " FAILED")
-		
