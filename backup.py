9480af2ada5ccdf9551fdc7da6e613cdca75f3e8
unknown <db@teamtroxler.com>
2020-09-03 01:52:53 -0400

    Attempt to build the SSH connection first before making the local file to store it in, in case of failed connection

--- backup.py
+++ backup.py
@@ -10,10 +10,10 @@ hosts = hosts.split()
 pw = getpass.getpass()
 for each in hosts:
 	try:
+		x = NTC(host=each, username='dtroxler', password=pw, device_type='cisco_ios_ssh')
 		file = open(each+".txt", "w+")
 		print("\n", each, "-- writing backup locally")
-		x = NTC(host=each, username='dtroxler', password=pw, device_type='cisco_ios_ssh')
 		backup = x.running_config
 		file.write(backup)
 	except:
-		print(each, " FAILED")
+		print(each, "CONNECTION FAILED")
